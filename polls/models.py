a4e7f85a33d96a0ea0444384381302a6ef4bc01a
ddanieela1 <vdaniela.gv@gmail.com>
2022-12-06 20:33:17 -0500

    updated index, views, test(part 5,6)

--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-        
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -22,4 +22,6 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse,HttpResponseRedirect
-from django.template import loader
-from django.shortcuts import render
+# from django.template import loader
 from django.urls import reverse
 from django.views import generic
 
+
 from .models import Question
+from .models import Choice
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -14,12 +16,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
+        return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
